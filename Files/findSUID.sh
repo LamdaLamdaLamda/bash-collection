f72c096d0d43762937c72f1b21e46f1548e917ff
LamdaLamdaLamda <25778959+LamdaLamdaLamda@users.noreply.github.com>
2021-02-04 22:00:11 +0100

    Fix for duration calculation.

--- Files/findSUID.sh
+++ Files/findSUID.sh
@@ -6,7 +6,7 @@
 #Author       : LamdaLamdaLamda
 #Email        : 25778959+LamdaLamdaLamda@users.noreply.github.com
 ###################################################################
-if test $(id -u -n) != "root"
+if test "$(id -u -n)" != "root"
     then
     echo -e  "\u2715 Missing root privileges..."
     exit 1
@@ -16,10 +16,10 @@ startTime=$SECONDS
 
 echo -e "\u2328 Searching for SUID-bits in / ..."
 
-for i in `find / -user root -perm -4000 -print 2>/dev/null`
+for i in $(find / -user root -perm -4000 -print 2>/dev/null)
 do
   echo -e "\u2714 Found SUID $i"
 done
 
-duration=$(( SECONDS - start ))
+duration=$(( SECONDS - startTime ))
 echo -e "\u2328 Done within $duration seconds"
