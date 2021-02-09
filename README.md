f555d79ac04a1105481c1011bde434d91b3de645
LamdaLamdaLamda <25778959+LamdaLamdaLamda@users.noreply.github.com>
2021-02-09 16:42:05 +0100

    README.md update + verify.sh issue exceptions explained.

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-# Bash-Collection
+# Bash-Collection [![Build Status](https://travis-ci.com/LamdaLamdaLamda/bash-collection.svg?branch=main)](https://travis-ci.com/LamdaLamdaLamda/bash-collection) [![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
+
 
 This  small collection contains runnable shell code, but also code-snippets (e.g Alias).
 These have the intention to improve your daily workflow.
 All shellscripts stick to the [Shell Style Guide](https://google.github.io/styleguide/shellguide.html) by Google (probably with some minor exceptions).
 
-Beware of running script you may not understood completly.
+Beware of running script you may not understood completely.
+
+## Contribution
+
+You want to contribute? Great!
+At first make sure you did installed **shellcheck**.
+If you contribute to this repository make sure you did run **verify.sh** successfully. This script will verify whether your contribution is valid shellscript or not.
+
+## Project structure
+
+**Alias** - All alias terminal commands belong here. A alias needs to be specified explicitly with the _alias_ keyword and does not accept any arguments.
+
+**Files** - File manipulation scripts.
+
+**Misc** - Script which are not categorized in any other directory.
+
+**Network** - Network related scripts.
+
+**Sec** - Script which are security or safety related. If a script touches any other category but is security related, it belongs in the Sec directory.
--- verify.sh
+++ verify.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 ###################################################################
-#Script Name  : verify.sh
-#Description  : Determines whether the repository contains valid shellscripts or not.
-#             : Returns 1 when any shellcheck failed, otherwise 0.
-#Args         : None
-#Author       : LamdaLamdaLamda
-#Email        : 25778959+LamdaLamdaLamda@users.noreply.github.com
+#Script Name    : verify.sh
+#Description    : Determines whether the repository contains valid shellscripts or not.
+#               : Returns 1 when any shellcheck failed, otherwise 0.
+#               : Some shellcheck metrics are excluded:
+#               :     SC2216 - Piping to 'rm', a command that doesn't read stdin.
+#               :              See: https://github.com/koalaman/shellcheck/wiki/SC2216
+#               :     SC2044 - For loops over find output are fragile.
+#               :              See: https://github.com/koalaman/shellcheck/wiki/SC2044
+#               :     SC2039 - In POSIX sh, something is undefined. Bash as interpreter is used.
+#               :              See: https://github.com/koalaman/shellcheck/wiki/SC2039
+#               :     SC2112 - 'function' keyword is non-standard. Bash as interpreter is used.
+#               :              See: https://github.com/koalaman/shellcheck/wiki/SC2112
+#               :     SC2002 - Useless cat. Consider 'cmd < file | ..' or 'cmd file | ..' instead. 
+#               :              See: https://github.com/koalaman/shellcheck/wiki/SC2002
+#               :     SC2010 - Don't use ls | grep. Use a glob or a for loop with a condition to allow non-alphanumeric filenames.
+#               :              See: https://github.com/koalaman/shellcheck/wiki/SC2010
+#               :     SC2129 - Consider using { cmd1; cmd2; } >> file instead of individual redirects.
+#               :              See: https://github.com/koalaman/shellcheck/wiki/SC2129
+#               :
+#Args           : None
+#Author         : LamdaLamdaLamda
+#Email          : 25778959+LamdaLamdaLamda@users.noreply.github.com
 ###################################################################
 ERROR=false
 function verify_directory {
@@ -23,18 +39,18 @@ function verify_directory {
     done
 }
 
-
 verify_directory "Files"
 verify_directory "Alias"
 verify_directory "Misc"
 verify_directory "Network"
 verify_directory "Sec"
 
-
 if $ERROR
 then
+    echo -e "[\u2715] Verification failed."
     exit 1
 else
+    echo -e "[\u2714] Verification suceed."
     exit 0
 fi
 
